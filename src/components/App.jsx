e6dcc692ac03a3ab2d71e21ec43016852ed56212
ysabelvalencia <ysavalencia@hotmail.com>
2023-10-20 19:11:20 +0200

    add basic htlm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,42 @@ import React from 'react';
 import '../styles/App.scss';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <header>Owen Wilson's Wow</header>
+      <main>
+        <form>
+          <label htmlFor="search-film">
+            Título de la película
+            <input
+              type="text"
+              name="search-film"
+              // value=""
+              // id=""
+              // onChange={}
+            />
+          </label>
+          <label htmlFor="search-year">
+            Año de la película
+            <select
+              name="search-year"
+              // id=""
+              // value={}
+              // onChange={}
+            >
+              <option selected value="">
+                Todos
+              </option>
+            </select>
+          </label>
+        </form>
+
+        <section>
+          <ul>Aqui se van a pintar los li con cada tarjeta</ul>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default App;
