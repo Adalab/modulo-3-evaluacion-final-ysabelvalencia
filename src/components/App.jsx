5bca4822f2b20127e012b230a3a30f8598f973d7
ysabelvalencia <ysavalencia@hotmail.com>
2023-10-20 19:59:49 +0200

    fetch with clean data from service

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
 import '../styles/App.scss';
+import getDataFromApi from '../services/api';
+import ls from '../services/localStorage';
+import { useEffect, useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { useLocation, matchPath } from 'react-router';
 
 const App = () => {
+  const [allMovies, setAllMovies] = useState([]);
+
+  useEffect(() => {
+    getDataFromApi().then((cleanData) => {
+      setAllMovies(cleanData);
+      console.log(cleanData);
+      // Con esto me aparece el listado limpio que me da el api con las 50 pelis.
+    });
+  }, []);
+
   return (
     <>
       <header>Owen Wilson's Wow</header>
@@ -25,7 +39,10 @@ const App = () => {
               // value={}
               // onChange={}
             >
-              <option selected value="">
+              <option
+              // selected
+              // value=""
+              >
                 Todos
               </option>
             </select>
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,20 @@
 const callToApi = () => {
-  return fetch('')
+  return fetch(
+    'https://owen-wilson-wow-api.onrender.com/wows/random?results=50'
+  )
     .then((response) => response.json())
-    .then((response) => {
-      return response;
+    .then((data) => {
+      const cleanData = data.map((data) => {
+        return {
+          movie: data.movie,
+          year: data.year,
+          director: data.director,
+          fullLine: data.full_line,
+          poster: data.poster,
+          audio: data.audio,
+        };
+      });
+      return cleanData;
     });
 };
 
